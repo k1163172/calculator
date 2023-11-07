d8dacfc475d127712b6a1d4f9678eafb60f19672
k1163172 <k1163172@naver.com>
2023-11-07 16:15:19 +0900

    숫자 버튼 기능(onPressNumber) 출력 구현

--- readme.md
+++ readme.md
@@ -5,10 +5,10 @@
 - [x] data 속성 추가
 - [x] calculator class 만들기
 - [x] 계산기 기능 관련 DOM 셋팅
-- [ ] 숫자 버튼 기능(onPressNumber) 구현
+- [x] 숫자 버튼 기능(onPressNumber) 출력 구현
+- [ ] 사칙연산 버튼 기능(onPressOperation) 출력 구현
 
 ### 2주차
-- [ ] 사칙연산 버튼 기능(onPressOperation) 구현
 - [ ] onEqual 기능 구현
 - [ ] onReset 기능 구현
 - [ ] onDelete 기능 구현
--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 class Calculator {
+    $previousPreview
+    $currentPreview
 
+    constructor($previousPreview, $currentPreview) {
+        this.$previousPreview = $previousPreview
+        this.$currentPreview = $currentPreview
+    }
+
+    onPressNumber(number) {
+        this.$currentPreview.textContent += number
+    }
 }
-const cal = new Calculator()
 
 const $previousPreview = document.querySelector('[data-previous-preview]')
 const $currentPreview = document.querySelector('[data-current-preview]')
-
 
 const $plus = document.querySelector('[data-btn-plus]')
 const $minus = document.querySelector('[data-btn-minus]')
@@ -14,6 +22,8 @@ const $divide = document.querySelector('[data-btn-divide]')
 
 const $numbers = document.querySelectorAll('[data-btn-number]')
 const $operations = document.querySelectorAll('[data-btn-operation]')
+
+const cal = new Calculator($previousPreview, $currentPreview)
 
 $numbers.forEach(($number) => {
     $number.addEventListener("click", (e) => {
