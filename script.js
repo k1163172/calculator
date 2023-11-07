604105af83dae75b6d9bba6d7e4cc57d6b8cd0db
k1163172 <k1163172@naver.com>
2023-11-07 15:42:22 +0900

    계산기 기능 관련 DOM 셋팅

--- script.js
+++ script.js
@@ -5,5 +5,24 @@ const cal = new Calculator()
 
 const $previousPreview = document.querySelector('[data-previous-preview]')
 const $currentPreview = document.querySelector('[data-current-preview]')
-console.log($previousPreview)
-console.log($currentPreview)
+
+
+const $plus = document.querySelector('[data-btn-plus]')
+const $minus = document.querySelector('[data-btn-minus]')
+const $multiply = document.querySelector('[data-btn-multiply]')
+const $divide = document.querySelector('[data-btn-divide]')
+
+const $numbers = document.querySelectorAll('[data-btn-number]')
+const $operations = document.querySelectorAll('[data-btn-operation]')
+
+$numbers.forEach(($number) => {
+    $number.addEventListener("click", (e) => {
+        console.log(e.target.textContent)
+    })
+})
+
+$operations.forEach(($operation) => {
+    $operation.addEventListener("click", (e) => {
+        console.log(e.target.textContent)
+    })
+})
